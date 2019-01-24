d0c2ba1b47f5fbb1ac9369764b5749cdfb94857e
Serhii Pirohov <sergio_89@ukr.net>
2019-01-24 18:01:36 +0200

    controller fixed

--- src/main/java/com/timecoder/web/SseController.java
+++ src/main/java/com/timecoder/web/SseController.java
@@ -3,17 +3,18 @@ package com.timecoder.web;
 import com.timecoder.service.SseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class SseController {
 
     private final SseService sseService;
 
-    @GetMapping(path = "/notifications", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @RequestMapping(value = "/stream", method = RequestMethod.POST, produces =  MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter getNotifications() {
         return sseService.registerEmitter(new SseEmitter());
     }
