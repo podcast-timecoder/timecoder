5df10e59a4e87dd2d3c1f43bf49df631b7ec96d0
Serhii_Pirohov <sergio_89@ukr.net>
2019-08-15 18:02:15 +0300

    IntegrationTests build fix

--- src/test/java/com/timecoder/tests/web/integraton/BaseIntegrationTest.java
+++ src/test/java/com/timecoder/tests/web/integraton/BaseIntegrationTest.java
@@ -6,14 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = TimecoderApplication.class)
-@AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+
 public class BaseIntegrationTest {
 
     public static String asJsonString(final Object obj) {
--- src/test/java/com/timecoder/tests/web/integraton/EpisodeControllerIntegrationTest.java
+++ src/test/java/com/timecoder/tests/web/integraton/EpisodeControllerIntegrationTest.java
@@ -1,11 +1,18 @@
 package com.timecoder.tests.web.integraton;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.timecoder.TimecoderApplication;
 import com.timecoder.dto.EpisodeDto;
+import com.timecoder.model.Episode;
 import com.timecoder.repository.EpisodeRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -13,7 +20,12 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = TimecoderApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class EpisodeControllerIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
@@ -32,11 +44,11 @@ public class EpisodeControllerIntegrationTest extends BaseIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.content().string("{\"created\":1}"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.created").exists());
     }
 
     @Test
-    public void testCanNotCreateEpisodeWithEmptyName() throws Exception {
+    public void testCanNotCreateEpisodeWithNullName() throws Exception {
         EpisodeDto episode = new EpisodeDto();
 
         mvc.perform(MockMvcRequestBuilders.post("/episodes")
@@ -44,5 +56,49 @@ public class EpisodeControllerIntegrationTest extends BaseIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest())
                 .andDo(print());
+    }
+
+    @Test
+    public void testCanNotCreateEpisodeWithBlankName() throws Exception {
+        EpisodeDto episode = new EpisodeDto();
+        episode.setName("");
+
+        mvc.perform(MockMvcRequestBuilders.post("/episodes")
+                .content(asJsonString(episode))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    public void testCanGetAllEpisodes() throws Exception {
+        long id = createTestEpisode("#1: Python").getId();
+
+        mvc.perform(MockMvcRequestBuilders.get("/episodes"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.content().json("[{\"id\":" + id + ",\"name\":\"#1: Python\",\"startTime\":null,\"themeList\":[],\"started\":false}]"));
+    }
+
+    @Test
+    public void testCanGetEpisodeById() throws Exception {
+        long id = createTestEpisode("#2: .Net").getId();
+
+        mvc.perform(MockMvcRequestBuilders.get("/episodes/" + id))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.content().json("{\"id\":"+id+",\"name\":\"#2: .Net\",\"startTime\":null,\"themeList\":[],\"started\":false}"));
+    }
+
+    private Episode createTestEpisode(String name) {
+        Episode episode = new Episode();
+        episode.setName(name);
+
+        return repository.save(episode);
+    }
+
+    @Before
+    public void deleteAllEpisodes() {
+        repository.deleteAll();
     }
 }
