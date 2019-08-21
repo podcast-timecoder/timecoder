e7b4b74058a0c75e69a451291a38c8010144aec5
Serhii_Pirohov <sergio_89@ukr.net>
2019-08-21 17:25:30 +0300

    remove unused imports

--- src/main/java/com/timecoder/handlers/RestExceptionHandler.java
+++ src/main/java/com/timecoder/handlers/RestExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
