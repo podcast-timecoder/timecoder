ebb880c061963a817a7e3f5100cc6f9320a6839b
sergey <sergio_89@ukr.net>
2020-04-03 15:35:34 +0300

    add xml report generation

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,5 @@
 java -jar jacoco/jacococli.jar dump --port=36322 --address=localhost --destfile=cov/result.exec
 
 java -jar jacoco/jacococli.jar report cov/result.exec --classfiles=./build/classes --sourcefiles=./src/main/java --html report
+
+java -jar jacoco/jacococli.jar report cov/result.exec --classfiles=./build/classes --sourcefiles=./src/main/java --xml report.xml
